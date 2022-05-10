22a7e82ec4441973cf4edb89d2d07d01a5229610
maria <mashak1407@gmail.com>
2022-05-10 18:10:12 +0300

    Add reading input data

--- app/Main.hs
+++ app/Main.hs
@@ -6,9 +6,27 @@ import Data.Maybe (isNothing)
 import Text.Read (readMaybe, readEither)
 
 
-
 main :: IO ()
-main = TODO()
+main = do
+    n <- getLine
+    if isNothing (readMaybe n :: Maybe Int) || read n < 1 
+        then putStr "'n' must be a natural number!\n"
+    else do   
+        args <- readArgs (read n) []
+        let res = reverse args
+        putStr $ show res ++ "\n"
 
 
+readArgs :: Int -> [[Int]] -> IO [[Int]]
+readArgs 0 xs = do
+    return xs
+readArgs n xs = do
+    lineArgs <- readLineArgs
+    readArgs (n - 1) (lineArgs : xs) 
+
+
+readLineArgs :: IO [Int]
+readLineArgs = do
+    s <- getLine
+    return $ map read $ words s
 
