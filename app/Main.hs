581f537e52334f1a42d71ccddb8bb4c4f0aa0c1b
maria <mashak1407@gmail.com>
2022-06-02 16:55:18 +0300

    Add input interface

--- app/Main.hs
+++ app/Main.hs
@@ -4,20 +4,34 @@ import Solver
 import Picosat (Solution(Unsatisfiable))
 import Data.Maybe (isNothing)
 import Text.Read (readMaybe, readEither)
+import System.Exit
+import Control.Monad
 
 
 main :: IO ()
 main = do
+    putStr "Enter the number of rows:\n"
     (numberOfRows, argsRows) <- readInputBlock
-    if numberOfRows == 0
-        then putStr "Invalid rows input! All elements must be natural numbers separated by spaces.\n"
-    else do     
-        (numberOfCols, argsCols) <- readInputBlock
-        if numberOfCols == 0
-            then putStr "Invalid columns input! All elements must be natural numbers separated by spaces.\n"    
-        else do    
-            putStr $ show argsRows ++ "\n"
-            putStr $ show argsCols ++ "\n"
+    when (numberOfRows == 0)
+        (putStr "Invalid rows input! All elements must be natural numbers separated by spaces.\n" >> 
+        exitWith ExitSuccess)  
+
+    putStr "Enter the number of columns:\n"
+    (numberOfCols, argsCols) <- readInputBlock
+    when (numberOfRows == 0)
+        (putStr "Invalid columns input! All elements must be natural numbers separated by spaces.\n" >> 
+        exitWith ExitSuccess)  
+
+    let countLongLine maxLen args = length $ filter (> maxLen) (map (foldl (\sum cur -> sum + cur + 1) (-1)) args)  
+    when (countLongLine numberOfCols argsRows /= 0)
+        (putStr "Invalid rows input! Some line is too long.\n" >> 
+        exitWith ExitSuccess)     
+    when (countLongLine numberOfRows argsCols /= 0)
+        (putStr "Invalid columns input! Some line is too long.\n" >> 
+        exitWith ExitSuccess)
+
+    putStr $ show argsRows ++ "\n"
+    putStr $ show argsCols ++ "\n"
 
 
 readInputBlock :: IO (Int, [[Int]])
@@ -26,6 +40,7 @@ readInputBlock = do
     if isNothing (readMaybe n :: Maybe Int) || read n < 1 
         then return (0, [[]])
     else do
+        putStr "Enter the specified number of lines:\n"
         revArgs <- readArgs (read n) []
         let args = reverse revArgs
         return $ checkAllNums (read n) args
